a4e590d8e9e971db1110ca5fc6a8c75203c9b957
akemilol <valeriaconcedsantos@gmail.com>
2025-09-10 17:07:28 -0300

    chore(types): adiciona declaração para firebase/auth/react-native

--- ListaTarefasPlus/src/firebase/app.ts
+++ ListaTarefasPlus/src/firebase/app.ts
@@ -1,15 +1,19 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { initializeAuth, getAuth } from 'firebase/auth'
+import { getReactNativePersistence } from 'firebase/auth/react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCHj89vd85966IQzmCJ7I4UjQ4151UEx8c',
-  authDomain: 'lista-tarefas-plus.firebaseapp.com',
-  projectId: 'lista-tarefas-plus',
-  storageBucket: 'lista-tarefas-plus.firebasestorage.app',
-  messagingSenderId: '708965482098',
-  appId: '1:708965482098:web:e454ea8cf8ce1f6681cceb'
+  apiKey: 'AIzaSyCydDGRWRmXH19jSA_dAlad5XQGKLOtMso',
+  authDomain: 'listartarefasplus.firebaseapp.com',
+  projectId: 'listartarefasplus',
+  storageBucket: 'listartarefasplus.appspot.com',
+  messagingSenderId: '103265643462',
+  appId: '1:103265643462:web:07442c710158a9c9dfa0c7'
 }
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+const auth = getApps().length ? getAuth(app) : initializeAuth(app, { persistence: getReactNativePersistence(AsyncStorage) })
+
+export { app, auth }
 export default app
